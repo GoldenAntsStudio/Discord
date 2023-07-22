fd724ea7488bf490f7cb9c87b3c2f1362f5ff2a3
Smart Ant Studio <hometikhon777@gmail.com>
2023-07-22 16:09:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import discord
+import time
+from gen_pass import gen_pass_test
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+coins = 20
+coinsPerMine = 10
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    global coins, coinsPerMine
+
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('/mine'):
+        coins += coinsPerMine
+        await message.channel.send(f"+{coinsPerMine}")
+
+    elif message.content.startswith('/balance'):
+        await message.channel.send(str(coins))
+    
+    elif message.content.startswith('/upgrade'):
+        if coins >= 20:
+            coins-=20
+            coinsPerMine+=10
+            await message.channel.send("Улучшен майнинг\n-20")
+        else:
+            await message.channel.send("Не хватает монет для улушения")
+
+    
+
+
+client.run("MTEzMjI3NzQxMDYzNzgyNDA1MQ.GJ-C21.7IM3haMdi4FxjWVprlMnbvyh-EkeBTSZDBsNME")
